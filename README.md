77ed373c3ef0585d7e8bf17559c97e42de420a54
Birk <rgeebt01@yahoo.com>
2022-04-19 14:59:34 -0400

    changed to double

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 # Description
 Program demonstrating arithmetic and operators
 
+# Update1
+Changed from int to double
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int result = 10/3;
+        double result = (double)10 / (double)3;
         System.out.println(result);
     }
 }
